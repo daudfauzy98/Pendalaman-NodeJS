a1058a70a35fe34c1686e7fa3eec012af7b7b3d9
daudfauzy98 <daudfauzy98@gmail.com>
2020-10-12 12:10:50 +0700

    handle upload files & serve files

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
+
 import { getProduct, iniDatabase, iniTable, insertProduct } from './database.js'
+import { fstat } from 'fs'
+//import { get } from 'http'
 
 const __dirname = path.resolve()
 const app = express()
@@ -15,6 +20,8 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+app.use(fileUpload())
+
 // Log incoming request
 app.use(morgan('combined'))
 
@@ -23,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false}))
 
 // Serve static filess
 app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
     res.send({ success: true })
@@ -61,8 +69,20 @@ app.get('/add-product', (req, res, next) => {
 // Handle from POST method
 app.post('/add-product', (req, res, next) => {
     console.log('Request', req.body)
-    insertProduct(db, req.body.name, parseInt(req.body.price), '-')
-    //res.send(req.body)
+    console.log('File', req.files)
+    // Get file name
+    const fileName = Date.now() + req.files.photo.name
+    // Write (simpan) file
+    fs.writeFile(path.join(__dirname, '/files/', fileName), req.files.photo.data, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+
+    // Insert product
+    insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
+    // Redirect
     res.redirect('/product')
 })
 
